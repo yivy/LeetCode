e533334e9f40bfb5cf97f05c477080f4ff082c69
mengwenyi <mengwenyi@100tal.com>
2020-04-20 18:38:39 +0800

    提交s11代码,求最大面积，使用双指针

---
+++ src/S11_maxArea.java
@@ -0,0 +1,59 @@
+/**
+ * @description:
+ * @author: mengwenyi
+ * @Date: 2020/4/20 17:52
+ */
+
+/**
+ * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ *
+ * 说明：你不能倾斜容器，且 n 的值至少为 2。
+ *
+ * 图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+ *
+ *  
+ *
+ * 示例：
+ *
+ * 输入：[1,8,6,2,5,4,8,3,7]
+ * 输出：49
+ */
+public class S11_maxArea {
+
+    public int maxArea(int[] height) {
+        int area=0;
+        for(int i=0;i<height.length;i++){
+            for(int j=i+1;j<height.length;j++){
+                int temarea = (j-i)*Math.min(height[i],height[j]);
+                if(temarea>area){
+                    area=temarea;
+                }
+            }
+        }
+        return area;
+    }
+    public int maxArea1(int[] height) {
+        int area=0;
+        int i=0;
+        int j=height.length-1;
+        while(i<j){
+            int temarea = (j-i)*Math.min(height[i],height[j]);
+            if(temarea>area){
+                area=temarea;
+            }
+            if(height[i]<=height[j]){//移动左指针
+                i++;
+            }else{//移动右指针
+                j--;
+            }
+        }
+        return area;
+    }
+    public static void main(String[] args) {
+        S11_maxArea s11_maxArea = new S11_maxArea();
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        int[] height1 = {1};
+
+        System.out.println(s11_maxArea.maxArea1(height1));
+    }
+}
